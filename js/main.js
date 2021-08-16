d138d7889c0bc9554b5db74752dcebb7eb2206d9
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2021-08-15 20:17:59 -0500

    mepkd

--- js/main.js
+++ js/main.js
@@ -155,13 +155,13 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdown-wed").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown-wed").innerHTML = "¡Gracias por asistir!";
   }
 }, 1000);
 
